390712379000adbf8e5cdac8f4e8eef66bc295f1
Vincent <>
2020-02-10 14:56:33 +0100

    gunicorn log

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config["TESTING"] = True
+app.config["TESTING"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["UPLOAD_FOLDER"] = ""
 # app.config["SEND_FILE_MAX_AGE_DEFAULT"] = timedelta(seconds=30)
